42dc5a3998c1fa8899c38f263c01b8bd358bec92
Pavani Reddy <kollamorampavani123@gmail.com>
2026-05-14 08:24:47 +0530

    Bind server to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -67,4 +67,4 @@ const initializeDB = async () => {
 initializeDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
