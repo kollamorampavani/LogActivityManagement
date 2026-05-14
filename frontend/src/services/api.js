f75a33f2c0553919a82f72a79684db31a0fd2264
Pavani Reddy <kollamorampavani123@gmail.com>
2026-05-14 09:28:11 +0530

    Update API URL to EC2 IP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://3.110.58.191:5000/api',
 });
 
 api.interceptors.request.use((config) => {
