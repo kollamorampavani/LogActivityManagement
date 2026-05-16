e95738e4a243db0c091ed7a4fb30c8d15aa66deb
Pavani Reddy <kollamorampavani123@gmail.com>
2026-05-16 13:03:29 +0530

    Fixed API base URL for subpath deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: window.location.pathname.startsWith('/pavani')
+        ? '/pavani/api'
+        : '/api',
 });
 
 api.interceptors.request.use((config) => {
     const user = JSON.parse(localStorage.getItem('user'));
+
     if (user && user.token) {
         config.headers.Authorization = `Bearer ${user.token}`;
     }
+
     return config;
 });
 
